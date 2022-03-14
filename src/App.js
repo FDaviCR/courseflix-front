02eb4d506ee676dd91dae38f68a7503bd123ef72
Davi <davicamiloribeiro@gmail.com>
2022-03-14 17:07:03 -0300

    Container base

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import HelloWorld from './components/HelloWorld';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Company from './pages/Company';
+
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        Olá React
-      </div>
-      <HelloWorld />
-    </div>
+    <Router>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Company</Link>
+      </ul>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
