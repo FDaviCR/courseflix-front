7f0ed99b6d5824a546283982a15d58f030a83a80
Davi <davicamiloribeiro@gmail.com>
2022-03-14 19:04:30 -0300

    Ajustes na Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import styles from './Home.module.css';
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>
+                Bem-vindo ao <span>Courseflix</span>
+            </h1>
+            <p>Veja nosso catalogo de cursos agora mesmo!</p>
+        </section>
+    );
 }
 
 export default Home;
